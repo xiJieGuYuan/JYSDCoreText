1fb41a82124cd39d25d0a606036ed64d476a2695
JiaoyangLiao <liaojy@litsoft.com.cn>
2018-04-20 10:06:23 +0800

    add libray

---
+++ JYSDCoreText/include/TestALibray/TestALibray.h
@@ -0,0 +1,18 @@
+//
+//  TestALibray.h
+//  TestALibray
+//
+//  Created by Mr  liao jia yang  on 2018/3/30.
+//  Copyright © 2018年 Mr  liao jia yang . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestALibray : NSObject
+/**
+ 打印出输入文字内容
+
+ @param name 输入的名字
+ */
+-(void)ouputStringWithInputName:(NSString *)name;
+@end
